88411c3cfbf36dd697459e62b95421e0381144b8
Mollie-S <olcha.olcha@gmail.com>
2021-01-18 21:20:33 +0100

    Typo in ifndef.

--- src/data.h
+++ src/data.h
@@ -6,11 +6,11 @@
 /*   By: osamara <osamara@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/12 18:58:39 by osamara       #+#    #+#                 */
-/*   Updated: 2021/01/12 19:17:22 by osamara       ########   odam.nl         */
+/*   Updated: 2021/01/18 21:10:05 by osamara       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifdef DATA_H
+#ifndef DATA_H
 # define DATA_H
 
 typedef struct		s_data
