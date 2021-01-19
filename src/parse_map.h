3bb8b7249240e35722939b7cac1bf23320521f45
Mollie-S <olcha.olcha@gmail.com>
2021-01-19 15:57:02 +0100

    Added define for the error.

--- src/parse_map.h
+++ src/parse_map.h
@@ -6,7 +6,7 @@
 /*   By: osamara <osamara@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/18 18:56:31 by osamara       #+#    #+#                 */
-/*   Updated: 2021/01/18 18:57:53 by osamara       ########   odam.nl         */
+/*   Updated: 2021/01/19 15:25:09 by osamara       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 # define PARSE_MAP_H
 
 # include "map.h"
+
+# define ERROR 0
 
 #endif
