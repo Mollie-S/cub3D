f7ffbf9a8d763b65824b6200f78829867fc89c6d
Mollie-S <olcha.olcha@gmail.com>
2021-01-20 19:15:58 +0100

    Updated variables in the struct s_map.

--- src/map.h
+++ src/map.h
@@ -6,12 +6,17 @@
 /*   By: osamara <osamara@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/18 12:53:44 by osamara       #+#    #+#                 */
-/*   Updated: 2021/01/19 17:51:25 by osamara       ########   odam.nl         */
+/*   Updated: 2021/01/20 16:58:29 by osamara       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAP_H
 # define MAP_H
+
+typedef struct		s_scene_descript
+{
+
+}					t_scene_descript;
 
 typedef struct		s_map
 {
@@ -20,6 +25,7 @@ typedef struct		s_map
 	int				start_direction;
 	double			start_pos_x;
 	double			start_pos_y;
+	char			*fields;
 }					t_map;
 
 
