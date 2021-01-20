b5156f89397dfaa9c24846564ea0c61020093b6b
Mollie-S <olcha.olcha@gmail.com>
2021-01-20 21:33:27 +0100

    Created additional structurus for scene description.

--- src/map.h
+++ src/map.h
@@ -6,7 +6,7 @@
 /*   By: osamara <osamara@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/18 12:53:44 by osamara       #+#    #+#                 */
-/*   Updated: 2021/01/20 21:17:33 by osamara       ########   odam.nl         */
+/*   Updated: 2021/01/20 21:32:37 by osamara       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,33 @@
 
 typedef struct		s_scene_descript
 {
+	char		*identifier;
 
+	int			resolution_x;
+	int			resolution_y;
+	char		*texture;
+	char		*no_texture;
+	char		*so_texture;
+	char		*ea_texture;
+	char		*we_texture;
+	char		*sprite_texture;
 }					t_scene_descript;
+
+typedef struct		s_color
+{
+	char	identifier;
+	int		*floor_rgb;
+	int		*ceiling_rgb;
+}					t_color;
 
 typedef struct		s_map
 {
-	int				height;
-	int				width;
-	int				start_direction;
-	double			start_pos_x;
-	double			start_pos_y;
-	char			*fields;
+	int			height;
+	int			width;
+	int			start_direction;
+	double		start_pos_x;
+	double		start_pos_y;
+	char		*fields;
 }					t_map;
 
 
