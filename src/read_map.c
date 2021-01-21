a09d3232f246dcbf66c97dc969c3eaf8f38fc088
Mollie-S <olcha.olcha@gmail.com>
2021-01-21 13:03:24 +0100

    Added a separate parsing error function.

--- src/read_map.c
+++ src/read_map.c
@@ -6,7 +6,7 @@
 /*   By: osamara <osamara@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/18 18:56:07 by osamara       #+#    #+#                 */
-/*   Updated: 2021/01/21 11:37:49 by osamara       ########   odam.nl         */
+/*   Updated: 2021/01/21 13:00:37 by osamara       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,23 +51,38 @@ int		read_from_file(int fd)
 		{
 			return (content_error(line));
 		}
-		// check for all the identifiers first.
-		// free line and assign it to null if it's not a map
-		if (!push_line_to_llist(&list_start, line))
+		if (!handle_lines_read(line, &list_start))
 		{
-			return (content_error(line));
-		}
-		if (!push_line_to_llist(&list_start, line))
-		{
-			return (content_error(line));
+			return (parsing_error(line));
 		}
 	}
 	if (!parse_map(list_start))
 	{
-		return (ERROR);
+		return (parsing_error(line));
 	}
 	// clear list after parsing the map: 
 // 	ft_lstclear(list_start, void (*del)(void*)) ???
+	return (SUCCESS);
+}
+
+int		handle_lines_read(char *line, t_list **list_start)
+{
+	if (map_style_descriptor(line))
+	{
+		if (!parse_map_style_descriptor(line))
+		{
+			return (parsing_error(line));
+		}
+		// check for all the identifiers first.
+		// free line and assign it to null if it's not a map
+	}
+	else
+	{
+		if (!push_line_to_llist(list_start, line))
+		{
+			return (content_error(line));
+		}
+	}
 	return (SUCCESS);
 }
 
@@ -84,9 +99,36 @@ int		push_line_to_llist(t_list **list_start, char *line)
 	return (1);
 }
 
+/*
+** If an identifier is found the line is parsed and line freed
+** If the line is empty it is immediately freed
+*/
+int		map_style_descriptor(char *line)
+{
+	int i;
+
+	i = 0;
+	if (line[0] == '\0')
+	{
+		return (1);
+	}
+	else if (line[0] == 'R' || line[0] == 'N' || line[0] == 'S' || line[0] == 'W'
+		|| line[0] == 'E' || line[0] == 'F' || line[0] == 'C')
+	{
+		return (1);
+	}
+	return (0);
+}
+
 int		content_error(char *line)
 {
 	write(1, "Can't read the content\n", 23);
 	free(line);
-	return(0);
+	return(ERROR);
+}
+int		parsing_error(char *line)
+{
+	write(1, "Invalid map\n", 12);
+	free(line);
+	return(ERROR);
 }
