2174f9ef7a0d9f2f5d2a9f6eb33c102fbe8fdace
Mollie-S <olcha.olcha@gmail.com>
2021-01-23 00:24:50 +0100

    Updated color parsing functionality.

--- src/parse_map_style.c
+++ src/parse_map_style.c
@@ -6,7 +6,7 @@
 /*   By: osamara <osamara@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/21 11:57:38 by osamara       #+#    #+#                 */
-/*   Updated: 2021/01/22 17:53:52 by osamara       ########   odam.nl         */
+/*   Updated: 2021/01/23 00:20:39 by osamara       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,9 @@
 
 int		parse_map_style_descriptor(char *line, int line_num)
 {
-	t_style style;
+	t_style 		style;
+	unsigned int	floor_color;
+	unsigned int	ceiling_color;
 
 	style = (t_style){ 0, 0, 0, 0, 0, 0, 0 };
 	if (has_identifier(line, "R "))
@@ -48,9 +50,9 @@ int		parse_map_style_descriptor(char *line, int line_num)
 	else if (has_identifier(line, "S "))
 		style.sprite_texture = parse_texture_path(line);
 	else if (has_identifier(line, "C "))
-		style.ceiling_rgb = parse_color(line, line_num);// how to deal with invalid colors?
+		style.ceiling_rgb = parse_color(line, &ceiling_color);// how to deal with invalid colors?
 	else if (has_identifier(line, "F "))
-		style.floor_rgb = parse_color(line, line_num);
+		style.floor_rgb = parse_color(line, &floor_color); // do I check them here or in a separate function?
 	free(line);
 	line = NULL;
 	return (SUCCESS);
@@ -78,36 +80,67 @@ char	*parse_texture_path(char *line)
 	return (texture_path);
 }
 
-int		parse_color(char *line, int line_num)
+int		parse_color(char *line, unsigned int *color)
+{
+	char	**array;
+	int		color_component;
+	int		red;
+	int		green;
+	int		blue;
+
+	array = ft_split(line, ',');
+	if (array == NULL || array[3] != NULL)
+	{
+		free_memory(array);
+		return (-1);
+	}
+	color_component = 0;
+	if (is_valid_color_component(array[0], &color_component))
+		red = color_component;
+	else
+		return (-1);
+	if (is_valid_color_component(array[1], &color_component))
+		green = color_component;
+	else
+		return (-1);
+	if (is_valid_color_component(array[2], &color_component))
+		blue = color_component;
+	else
+		return (-1);
+	free_memory(array);
+	return (red << 16 | green << 8 | blue);
+}
+
+int		is_valid_color_component(const char *string, int *color_component)
+{
+	int		num_chars;
+	int		accumulator;
+	int		is_valid_color;
+	char 	*trimmed_string;
+
+	num_chars = 0;
+	is_valid_color = 1;
+	trimmed_string = ft_strtrim(string, ' ');
+	accumulator = ft_printf_atoi(trimmed_string, &num_chars);
+	if (trimmed_string[num_chars] != 0 || accumulator >= 0 && accumulator <= 255)
+	{
+		is_valid_color = -1;
+	}
+	*color_component = accumulator;
+	free(trimmed_string);
+	return (is_valid_color);
+}
+
+//is it possible to do it like this???
+void		free_memory(char **array)
 {
 	int i;
-	int	red;
-	int green;
-	int blue;
-	int num_chars;
-	// ints or unsigned ints?
+
 	i = 0;
-	red = -1;
-	green = -1;
-	blue = -1;
-	while (line[i] != 0)
+	while (array[i] != NULL)
 	{
-		num_chars = 0;
-		if ((line[i] = ' ' || ft_isdigit(line[i])) && red < 0)
-			red = ft_printf_atoi(line, &num_chars);
-		else if ((line[i] = ' ' || ft_isdigit(line[i])) && green < 0)
-			green = ft_printf_atoi(line + i, &num_chars);
-		else if ((line[i] = ' ' || ft_isdigit(line[i])) && blue < 0)
-			blue = ft_printf_atoi(line + i, &num_chars);
-		else if (line[i] = ',')
-			i++;
-		i += num_chars;
+		free(array[i]);
+		i++;
 	}
-	if ((red >= 0 && red >= 255)
-		&& (green >= 0 && green >= 255)
-		&& (blue >= 0 && blue >= 255))
-		return (red << 16 | green << 8 | blue);
-	// how to differentiate error and possible return value 0?
-	else
-		return (report_error(line_num, "Invalid color input"));
+	free(array);
 }
