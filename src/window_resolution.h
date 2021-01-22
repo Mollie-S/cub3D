3c224d9d674366b1754d44611cc094034e146c62
Mollie-S <olcha.olcha@gmail.com>
2021-01-22 12:41:36 +0100

    Renamed the header and the structure from window_props to window_resolution.

--- src/window_resolution.h
+++ src/window_resolution.h
@@ -1,22 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   window_props.h                                     :+:    :+:            */
+/*   window_resolution.h                                :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: osamara <osamara@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/20 21:47:56 by osamara       #+#    #+#                 */
-/*   Updated: 2021/01/20 21:50:49 by osamara       ########   odam.nl         */
+/*   Updated: 2021/01/22 09:54:33 by osamara       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WINDOW_PROPS_H
-# define WINDOW_PROPS_H
+#ifndef WINDOW_RESOLUTION_H
+# define WINDOW_RESOLUTION_H
 
-typedef struct      s_window_props
+typedef struct      s_window_resolution
 {
     int				resolution_x;
     int				resolution_y;
-}                   t_window_props;
+}                   t_window_resolution;
 
 #endif
