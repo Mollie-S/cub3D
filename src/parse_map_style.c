78f278d557419cf715224229e6ff825a8df1eaec
Mollie-S <olcha.olcha@gmail.com>
2021-01-22 17:36:09 +0100

    Parsing colors for the floor and ceiling.

--- src/parse_map_style.c
+++ src/parse_map_style.c
@@ -6,7 +6,7 @@
 /*   By: osamara <osamara@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/21 11:57:38 by osamara       #+#    #+#                 */
-/*   Updated: 2021/01/22 12:28:20 by osamara       ########   odam.nl         */
+/*   Updated: 2021/01/22 17:34:00 by osamara       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,6 @@ int		parse_map_style_descriptor(char *line, int line_num)
 	t_style style;
 
 	style = (t_style){ 0, 0, 0, 0, 0, 0, 0 };
-
 	if (has_identifier(line, "R "))
 	{
 		if (!parse_window_resolution(line, line_num))
@@ -52,8 +51,6 @@ int		parse_map_style_descriptor(char *line, int line_num)
 		style.ceiling_rgb = parse_color(line);
 	else if (has_identifier(line, "F "))
 		style.floor_rgb = parse_color(line);
-	// check for all the identifiers first.
-	// free line and assign it to null if it's not a map
 	free(line);
 	line = NULL;
 	return (SUCCESS);
@@ -79,4 +76,48 @@ char	*parse_texture_path(char *line)
 	}
 	texture_path = ft_strtrim(line + i, ' ');
 	return (texture_path);
+}
+
+int		parse_color(char *line)
+{
+	int i;
+	int	red;
+	int green;
+	int blue;
+	int num_chars;
+	// how to deal with the colors being 0,0,0 ? how to check that the color is valid?
+	i = 0;
+	red = -1;
+	green = -1;
+	blue = -1;
+	while (line[i] != 0)
+	{
+		num_chars = 0;
+		if ((line[i] = ' ' || ft_isdigit(line[i])) && red < 0)
+		{
+			red = ft_printf_atoi(line, &num_chars);
+		}
+		else if ((line[i] = ' ' || ft_isdigit(line[i])) && green < 0)
+		{
+			green = ft_printf_atoi(line + i, &num_chars);
+		}
+		else if ((line[i] = ' ' || ft_isdigit(line[i])) && blue < 0)
+		{
+			blue = ft_printf_atoi(line + i, &num_chars);
+		}
+		else if (line[i] = ',')
+		{
+			i++;
+		}
+		i += num_chars;
+	}
+	if (red >= 0 && green >= 0 && blue >= 0)
+	{
+		return (red << 16 | green << 8 | blue);
+	}
+	else
+	{
+		return (ERROR); // how to differentiate error and possible return value 0?
+	}
+
 }
