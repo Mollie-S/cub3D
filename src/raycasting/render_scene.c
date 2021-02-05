06a8fa858266a69d78a76aea9534e0b272b3833c
Mollie-S <olcha.olcha@gmail.com>
2021-02-05 12:31:46 +0100

    Added printing function for debug purposes.

--- src/raycasting/render_scene.c
+++ src/raycasting/render_scene.c
@@ -6,7 +6,7 @@
 /*   By: osamara <osamara@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/31 22:25:09 by osamara       #+#    #+#                 */
-/*   Updated: 2021/02/04 21:15:34 by osamara       ########   odam.nl         */
+/*   Updated: 2021/02/05 12:19:38 by osamara       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,27 +52,27 @@ double			distance_to_wall(t_intersection *intersection, t_engine_state *engine_s
 
 double		calc_dist_on_horiz_inters(t_engine_state *engine_state, t_map *map)
 {
-	t_intersection	horiz_intersection;
+	t_intersection	horiz_inters;
 
-	horiz_intersection.y = floor(engine_state->pos_y) - TILE_SIZE;
-	horiz_intersection.step_y = -TILE_SIZE;
+	horiz_inters.y = floor(engine_state->pos_y) - TILE_SIZE;
+	horiz_inters.step_y = -TILE_SIZE;
 	if (engine_state->ray_angle >= 90 && engine_state->ray_angle < 270)
 	{
-		horiz_intersection.y = floor(engine_state->pos_y) + TILE_SIZE;
-		horiz_intersection.step_y = TILE_SIZE;
+		horiz_inters.y = floor(engine_state->pos_y) + TILE_SIZE;
+		horiz_inters.step_y = TILE_SIZE;
 	}
-	horiz_intersection.x = engine_state->pos_x + fabs((engine_state->pos_y - horiz_intersection.y) * tan(engine_state->radian));
-	horiz_intersection.step_x = TILE_SIZE * fabs(tan(engine_state->radian));
+	horiz_inters.x = engine_state->pos_x + fabs((engine_state->pos_y - horiz_inters.y) * tan(engine_state->radian));
+	horiz_inters.step_x = TILE_SIZE * fabs(tan(engine_state->radian));
 	if (engine_state->ray_angle >= 180 && engine_state->ray_angle < 360)
 	{
-		horiz_intersection.x = engine_state->pos_x - fabs((engine_state->pos_y - horiz_intersection.y) * tan(engine_state->radian));
-		horiz_intersection.step_x = -horiz_intersection.step_x;
+		horiz_inters.x = engine_state->pos_x - fabs((engine_state->pos_y - horiz_inters.y) * tan(engine_state->radian));
+		horiz_inters.step_x = -horiz_inters.step_x;
 	}
-	printf("horiz_x_step: %g\n", horiz_intersection.step_x);
-	printf("horiz_y_step: %g\n", horiz_intersection.step_y);
-	printf("horiz_intersection.x:%g\n", horiz_intersection.x);
-	printf("horiz_intersection.y:%g\n", horiz_intersection.y);
-	return (distance_to_wall(&horiz_intersection, engine_state, map));
+	printf("horiz_x_step: %g\n", horiz_inters.step_x);
+	printf("horiz_y_step: %g\n", horiz_inters.step_y);
+	printf("horiz_inters.x:%g\n", horiz_inters.x);
+	printf("horiz_inters.y:%g\n", horiz_inters.y);
+	return (distance_to_wall(&horiz_inters, engine_state, map));
 }
 
 /*
@@ -92,42 +92,29 @@ double		calc_dist_on_horiz_inters(t_engine_state *engine_state, t_map *map)
 
 double		calc_dist_on_vert_inters(t_engine_state *engine_state, t_map *map)
 {
-	t_intersection	vert_intersection;
+	t_intersection	vert_inters;
 
-	vert_intersection.x = floor(engine_state->pos_x) + TILE_SIZE;
-	vert_intersection.step_x = TILE_SIZE;
+	vert_inters.x = floor(engine_state->pos_x) + TILE_SIZE;
+	vert_inters.step_x = TILE_SIZE;
 	if (engine_state->ray_angle >= 180 && engine_state->ray_angle < 360)
 	{
-		vert_intersection.x = floor(engine_state->pos_x) - TILE_SIZE;
-		vert_intersection.step_x = -vert_intersection.step_x;
+		vert_inters.x = floor(engine_state->pos_x) - TILE_SIZE;
+		vert_inters.step_x = -vert_inters.step_x;
 	}
-	vert_intersection.y = engine_state->pos_y - fabs((engine_state->pos_x - vert_intersection.x) / tan(engine_state->radian));
-	vert_intersection.step_y = -TILE_SIZE / fabs(tan(engine_state->radian));
+	vert_inters.y = engine_state->pos_y - fabs((engine_state->pos_x - vert_inters.x) / tan(engine_state->radian));
+	vert_inters.step_y = -TILE_SIZE / fabs(tan(engine_state->radian));
 	if (engine_state->ray_angle >= 90 && engine_state->ray_angle < 270)
 	{
-		vert_intersection.y = engine_state->pos_y + fabs((engine_state->pos_x - vert_intersection.x) / tan(engine_state->radian));
-		vert_intersection.step_y = -vert_intersection.step_y;
+		vert_inters.y = engine_state->pos_y + fabs((engine_state->pos_x - vert_inters.x) / tan(engine_state->radian));
+		vert_inters.step_y = -vert_inters.step_y;
 	}
-	printf("vert_x_step: %g\n", vert_intersection.step_x);
-	printf("vert_y_step: %g\n", vert_intersection.step_y);
-	printf("vert_x:%g\n", vert_intersection.x);
-	printf("vert_y:%g\n", vert_intersection.y);
-	return (distance_to_wall(&vert_intersection, engine_state, map));
+	printf("vert_x_step: %g\n", vert_inters.step_x);
+	printf("vert_y_step: %g\n", vert_inters.step_y);
+	printf("vert_x:%g\n", vert_inters.x);
+	printf("vert_y:%g\n", vert_inters.y);
+	return (distance_to_wall(&vert_inters, engine_state, map));
 }
 
-//remove this function!
-void		print_data(t_engine_state *engine_state,
-	t_intersection_result *inters_result, t_map *map)
-{
-
-	printf("ray angle: %g\n", engine_state->ray_angle);
-	printf("pos_x: %g\n", engine_state->pos_x);
-	printf("pos_y: %g\n", engine_state->pos_y);
-	printf("dist_horiz: %g\n", dist_horiz);
-	printf("dist_vert: %g\n", dist_vert);
-
-
-}
 init_intersection_result(t_intersection_result *inters_result)
 {
 	inters_result->dist_to_wall = 0;
@@ -147,6 +134,17 @@ void		init_engine_state(t_engine_state *engine_state, t_map *map)
 	engine_state->pos_y = map->start_pos_y + 0.5;
 	engine_state->ray_angle = map->start_direction - engine_state->FOV / 2.0;
 	engine_state->radian = engine_state->ray_angle * (M_PI / 180) + (double)(0.0001);
+}
+
+//remove this function!
+void		print_data(t_engine_state *engine_state,
+	t_intersection_result *inters_result, t_map *map)
+{
+
+	printf("ray angle: %g\n", engine_state->ray_angle);
+	printf("pos_x: %g\n", engine_state->pos_x);
+	printf("pos_y: %g\n", engine_state->pos_y);
+	printf("dist_horiz: %g\n", inters_result->dist_to_wall);
 }
 
 void		render_scene(t_map *map)
