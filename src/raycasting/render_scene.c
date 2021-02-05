4ffdca42733f5fc0e775ce5c9d367b15dfc5da92
Mollie-S <olcha.olcha@gmail.com>
2021-02-05 12:17:10 +0100

    Removed the obsolete intermediate step for walls distance calculating.

--- src/raycasting/render_scene.c
+++ src/raycasting/render_scene.c
@@ -6,7 +6,7 @@
 /*   By: osamara <osamara@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/31 22:25:09 by osamara       #+#    #+#                 */
-/*   Updated: 2021/02/04 18:19:53 by osamara       ########   odam.nl         */
+/*   Updated: 2021/02/04 21:15:34 by osamara       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,10 @@
 */
 
 
-double			distance_to_wall(t_intersection *intersection, t_engine_state *engine_state, t_map *map, double radian)
+double			distance_to_wall(t_intersection *intersection, t_engine_state *engine_state, t_map *map)
 {
 	size_t		field_index;
 	double		distance;
-
 
 	distance = 0;
 	while (intersection->x < map->width && intersection->y < map->height)
@@ -42,7 +41,7 @@ double			distance_to_wall(t_intersection *intersection, t_engine_state *engine_s
 		field_index = map->width * (size_t)intersection->y + (size_t)intersection->x;
 		if (map->fields[field_index] == FIELD_WALL)
 		{
-			distance = fabs((engine_state->pos_x - intersection->x) / cos(radian)); // if cos(radian) == 0? it should be 1
+			distance = fabs((engine_state->pos_x - intersection->x) / cos(engine_state->radian));
 			break ;
 		}
 		intersection->x += intersection->step_x;
@@ -51,29 +50,29 @@ double			distance_to_wall(t_intersection *intersection, t_engine_state *engine_s
 	return (distance);
 }
 
-double		calc_dist_on_horiz_inters(t_engine_state *engine_state, t_map *map, double ray_angle, double radian)
+double		calc_dist_on_horiz_inters(t_engine_state *engine_state, t_map *map)
 {
 	t_intersection	horiz_intersection;
 
 	horiz_intersection.y = floor(engine_state->pos_y) - TILE_SIZE;
 	horiz_intersection.step_y = -TILE_SIZE;
-	if (ray_angle >= 90 && ray_angle < 270)
+	if (engine_state->ray_angle >= 90 && engine_state->ray_angle < 270)
 	{
 		horiz_intersection.y = floor(engine_state->pos_y) + TILE_SIZE;
 		horiz_intersection.step_y = TILE_SIZE;
 	}
-	horiz_intersection.x = engine_state->pos_x + fabs((engine_state->pos_y - horiz_intersection.y) * tan(radian));
-	horiz_intersection.step_x = TILE_SIZE * fabs(tan(radian));
-	if (ray_angle >= 180 && ray_angle < 360)
+	horiz_intersection.x = engine_state->pos_x + fabs((engine_state->pos_y - horiz_intersection.y) * tan(engine_state->radian));
+	horiz_intersection.step_x = TILE_SIZE * fabs(tan(engine_state->radian));
+	if (engine_state->ray_angle >= 180 && engine_state->ray_angle < 360)
 	{
-		horiz_intersection.x = engine_state->pos_x - fabs((engine_state->pos_y - horiz_intersection.y) * tan(radian));
+		horiz_intersection.x = engine_state->pos_x - fabs((engine_state->pos_y - horiz_intersection.y) * tan(engine_state->radian));
 		horiz_intersection.step_x = -horiz_intersection.step_x;
 	}
 	printf("horiz_x_step: %g\n", horiz_intersection.step_x);
 	printf("horiz_y_step: %g\n", horiz_intersection.step_y);
 	printf("horiz_intersection.x:%g\n", horiz_intersection.x);
 	printf("horiz_intersection.y:%g\n", horiz_intersection.y);
-	return (distance_to_wall(&horiz_intersection, engine_state, map, radian));
+	return (distance_to_wall(&horiz_intersection, engine_state, map));
 }
 
 /*
@@ -91,79 +90,81 @@ double		calc_dist_on_horiz_inters(t_engine_state *engine_state, t_map *map, doub
 **
 */
 
-double		calc_dist_on_vert_inters(t_engine_state *engine_state, t_map *map, double ray_angle, double radian)
+double		calc_dist_on_vert_inters(t_engine_state *engine_state, t_map *map)
 {
 	t_intersection	vert_intersection;
 
 	vert_intersection.x = floor(engine_state->pos_x) + TILE_SIZE;
 	vert_intersection.step_x = TILE_SIZE;
-	if (ray_angle >= 180 && ray_angle < 360)
+	if (engine_state->ray_angle >= 180 && engine_state->ray_angle < 360)
 	{
 		vert_intersection.x = floor(engine_state->pos_x) - TILE_SIZE;
 		vert_intersection.step_x = -vert_intersection.step_x;
 	}
-	vert_intersection.y = engine_state->pos_y - fabs((engine_state->pos_x - vert_intersection.x) / tan(radian));
-	vert_intersection.step_y = -TILE_SIZE / fabs(tan(radian));
-	if (ray_angle >= 90 && ray_angle < 270)
+	vert_intersection.y = engine_state->pos_y - fabs((engine_state->pos_x - vert_intersection.x) / tan(engine_state->radian));
+	vert_intersection.step_y = -TILE_SIZE / fabs(tan(engine_state->radian));
+	if (engine_state->ray_angle >= 90 && engine_state->ray_angle < 270)
 	{
-		vert_intersection.y = engine_state->pos_y + fabs((engine_state->pos_x - vert_intersection.x) / tan(radian));
+		vert_intersection.y = engine_state->pos_y + fabs((engine_state->pos_x - vert_intersection.x) / tan(engine_state->radian));
 		vert_intersection.step_y = -vert_intersection.step_y;
 	}
 	printf("vert_x_step: %g\n", vert_intersection.step_x);
 	printf("vert_y_step: %g\n", vert_intersection.step_y);
 	printf("vert_x:%g\n", vert_intersection.x);
 	printf("vert_y:%g\n", vert_intersection.y);
-	return (distance_to_wall(&vert_intersection, engine_state, map, radian));
+	return (distance_to_wall(&vert_intersection, engine_state, map));
 }
 
-double		calculate_dist_to_wall(t_engine_state *engine_state, t_map *map, double ray_angle, double radian)
+//remove this function!
+void		print_data(t_engine_state *engine_state,
+	t_intersection_result *inters_result, t_map *map)
 {
-	double	dist_horiz;
-	double	dist_vert;
 
-	printf("ray angle: %g\n", ray_angle);
+	printf("ray angle: %g\n", engine_state->ray_angle);
 	printf("pos_x: %g\n", engine_state->pos_x);
 	printf("pos_y: %g\n", engine_state->pos_y);
-	dist_horiz = calc_dist_on_horiz_inters(engine_state, map, ray_angle, radian);
-	dist_vert = calc_dist_on_vert_inters(engine_state, map, ray_angle, radian);
 	printf("dist_horiz: %g\n", dist_horiz);
 	printf("dist_vert: %g\n", dist_vert);
-	if (dist_horiz < dist_vert)
-		return (dist_horiz);
-	return (dist_vert);
+
+
 }
-
-// struct intersection_result
-// {
-// 	distance;
-
-// pointer_to the texture;
-// texture_coordinate;
-
-// };
-
-
-void		init_engine_state(t_engine_state *engine_state, t_map *map)
+init_intersection_result(t_intersection_result *inters_result)
 {
-	engine_state->FOV = 60.0;
-	engine_state->pos_x = map->start_pos_x + 0.5;
-	engine_state->pos_y = map->start_pos_y + 0.5;
+	inters_result->dist_to_wall = 0;
+	inters_result->is_side_wall = 0;
+	inters_result->current_texture = NULL;
+	inters_result->texture_x = 0;
+	inters_result->texture_y = 0;
 }
 
 /*
 **	adding (double)(0.0001) to radian to avoid division by 0 in case with tan(radian) or cos(radian)
 */
+void		init_engine_state(t_engine_state *engine_state, t_map *map)
+{
+	engine_state->FOV = 60.0;
+	engine_state->pos_x = map->start_pos_x + 0.5;
+	engine_state->pos_y = map->start_pos_y + 0.5;
+	engine_state->ray_angle = map->start_direction - engine_state->FOV / 2.0;
+	engine_state->radian = engine_state->ray_angle * (M_PI / 180) + (double)(0.0001);
+}
 
 void		render_scene(t_map *map)
 {
-	t_engine_state	engine_state;
-	double			ray_angle;
-	double			radian;
-	double			dist_to_wall;
+	t_engine_state			engine_state;
+	t_intersection			*intersection;
+	t_intersection_result	*inters_result;
+	double					dist_hr_inters;
+	double					dist_ver_inters;
 
 	init_engine_state(&engine_state, map);
-	ray_angle = map->start_direction - engine_state.FOV / 2.0;
-	radian = ray_angle * (M_PI / 180) + (double)(0.0001);
+	init_intersection_result(&inters_result);
 	// I need to loop n times (pix per pix through resolution )to draw lines
-	dist_to_wall = calculate_dist_to_wall(&engine_state, map, ray_angle, radian);
-}
+	inters_result->dist_to_wall = calculate_dist_to_wall(&engine_state, &inters_result, map);
+	dist_hr_inters = calc_dist_on_horiz_inters(&engine_state, map);
+	dist_ver_inters = calc_dist_on_vert_inters(&engine_state, map);
+	if (dist_ver_inters < dist_hr_inters)
+	{
+		inters_result->is_side_wall = 1;
+		if (intersection->) // how to check if the wall is right or left??
+	}
--- src/raycasting/render_scene.h
+++ src/raycasting/render_scene.h
@@ -6,26 +6,18 @@
 /*   By: osamara <osamara@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/31 22:26:12 by osamara       #+#    #+#                 */
-/*   Updated: 2021/02/04 17:39:48 by osamara       ########   odam.nl         */
+/*   Updated: 2021/02/04 20:06:16 by osamara       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RENDER_SCENE_H
 # define RENDER_SCENE_H
 
-#include "engine_state.h"
-#include "map.h"
-
-typedef struct      s_intersection
-{
-    double x;
-    double y;
-    double step_x;
-    double step_y;
-}                   t_intersection;
+# include "engine_state.h"
+# include "map.h"
+# include "intersection.h"
 
 void		render_scene(t_map *map);
 void	init_engine_state(t_engine_state *engine_state, t_map *map);
-
 
 #endif
