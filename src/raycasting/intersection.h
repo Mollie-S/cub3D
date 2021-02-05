54b3a83ff40ae5be0fc90d11a5e7cdd68be28b4f
Mollie-S <olcha.olcha@gmail.com>
2021-02-05 12:15:06 +0100

    Added an intersection_result structure.

---
+++ src/raycasting/intersection.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   intersection.h                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: osamara <osamara@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/02/04 20:04:19 by osamara       #+#    #+#                 */
+/*   Updated: 2021/02/04 20:28:34 by osamara       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INTERSECTION_H
+# define INTERSECTION_H
+
+# include <stddef.h>
+
+typedef struct      s_intersection
+{
+	double	x;
+	double	y;
+	double	step_x;
+	double	step_y;
+}                   t_intersection;
+
+typedef struct      s_intersection_result
+{
+	double	dist_to_wall;
+	int		is_side_wall;
+	char	*current_texture;
+	size_t	texture_x;
+	size_t	texture_y;
+}					t_intersection_result;
+
+
+#endif
