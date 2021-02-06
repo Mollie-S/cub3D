0cca63041c8eaa5f9d9ebe0805f790b99b3d9de1
Mollie-S <olcha.olcha@gmail.com>
2021-02-07 00:04:08 +0100

    Update.

--- src/raycasting/engine_state.h
+++ src/raycasting/engine_state.h
@@ -6,14 +6,12 @@
 /*   By: osamara <osamara@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/31 22:26:16 by osamara       #+#    #+#                 */
-/*   Updated: 2021/02/06 22:09:57 by osamara       ########   odam.nl         */
+/*   Updated: 2021/02/07 00:01:10 by osamara       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENGINE_STATE_H
 # define ENGINE_STATE_H
-
-# define TILE_SIZE 1
 
 typedef struct		s_engine_state
 {
--- src/raycasting/render_scene.c
+++ src/raycasting/render_scene.c
@@ -6,7 +6,7 @@
 /*   By: osamara <osamara@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/31 22:25:09 by osamara       #+#    #+#                 */
-/*   Updated: 2021/02/06 22:08:21 by osamara       ########   odam.nl         */
+/*   Updated: 2021/02/07 00:03:22 by osamara       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,31 +54,29 @@ double			distance_to_wall(t_intersection *intersection, t_engine_state *engine_s
 	return (distance);
 }
 
-double		dist_to_hor_inters(t_engine_state *engine_state, t_map *map)
+double		dist_to_hor_inters(t_engine_state *engine_state, t_intersection *hor_inters, t_map *map)
 {
-	t_intersection	hor_inters;
-
-	hor_inters.y = floor(engine_state->pos_y) - TILE_SIZE;
-	hor_inters.step_y = -TILE_SIZE;
+	hor_inters->y = floor(engine_state->pos_y);
+	hor_inters->step_y = -TILE_SIZE;
 	if (engine_state->ray_angle >= 90 && engine_state->ray_angle < 270)
 	{
-		hor_inters.y = floor(engine_state->pos_y) + TILE_SIZE;
-		hor_inters.step_y = TILE_SIZE;
+		hor_inters->y = floor(engine_state->pos_y) + TILE_SIZE;
+		hor_inters->step_y = TILE_SIZE;
 	}
-	hor_inters.x = engine_state->pos_x + fabs((engine_state->pos_y - hor_inters.y)
+	hor_inters->x = engine_state->pos_x + fabs((engine_state->pos_y - hor_inters->y)
 		* tan(DEG2RAD(engine_state->ray_angle)));
-	hor_inters.step_x = TILE_SIZE * fabs(tan(DEG2RAD(engine_state->ray_angle)));
+	hor_inters->step_x = TILE_SIZE * fabs(tan(DEG2RAD(engine_state->ray_angle)));
 	if (engine_state->ray_angle >= 180 && engine_state->ray_angle < 360)
 	{
-		hor_inters.x = engine_state->pos_x - fabs((engine_state->pos_y - hor_inters.y)
+		hor_inters->x = engine_state->pos_x - fabs((engine_state->pos_y - hor_inters->y)
 			* tan(DEG2RAD(engine_state->ray_angle)));
-		hor_inters.step_x = -hor_inters.step_x;
+		hor_inters->step_x = -hor_inters->step_x;
 	}
-	// printf("horiz_x_step: %g\n", hor_inters.step_x);
-	// printf("horiz_y_step: %g\n", hor_inters.step_y);
-	// printf("hor_inters.x:%g\n", hor_inters.x);
-	// printf("hor_inters.y:%g\n", hor_inters.y);
-	return (distance_to_wall(&hor_inters, engine_state, map));
+	// printf("horiz_x_step: %g\n", hor_inters->step_x);
+	// printf("horiz_y_step: %g\n", hor_inters->step_y);
+	// printf("hor_inters->x:%g\n", hor_inters->x);
+	// printf("hor_inters->y:%g\n", hor_inters->y);
+	return (distance_to_wall(hor_inters, engine_state, map));
 }
 
 /*
@@ -96,31 +94,29 @@ double		dist_to_hor_inters(t_engine_state *engine_state, t_map *map)
 **
 */
 
-double		dist_to_ver_inters(t_engine_state *engine_state, t_map *map)
+double		dist_to_ver_inters(t_engine_state *engine_state, t_intersection *vert_inters, t_map *map)
 {
-	t_intersection	vert_inters;
-
-	vert_inters.x = floor(engine_state->pos_x) + TILE_SIZE;
-	vert_inters.step_x = TILE_SIZE;
+	vert_inters->x = floor(engine_state->pos_x) + TILE_SIZE;
+	vert_inters->step_x = TILE_SIZE;
 	if (engine_state->ray_angle >= 180 && engine_state->ray_angle < 360)
 	{
-		vert_inters.x = floor(engine_state->pos_x) - TILE_SIZE;
-		vert_inters.step_x = -vert_inters.step_x;
+		vert_inters->x -= TILE_SIZE;
+		vert_inters->step_x = -vert_inters->step_x;
 	}
-	vert_inters.y = engine_state->pos_y - fabs((engine_state->pos_x - vert_inters.x)
+	vert_inters->y = engine_state->pos_y - fabs((engine_state->pos_x - vert_inters->x)
 		/ tan(DEG2RAD(engine_state->ray_angle)));
-	vert_inters.step_y = -TILE_SIZE / fabs(tan(DEG2RAD(engine_state->ray_angle)));
+	vert_inters->step_y = -TILE_SIZE / fabs(tan(DEG2RAD(engine_state->ray_angle)));
 	if (engine_state->ray_angle >= 90 && engine_state->ray_angle < 270)
 	{
-		vert_inters.y = engine_state->pos_y + fabs((engine_state->pos_x - vert_inters.x)
+		vert_inters->y = engine_state->pos_y + fabs((engine_state->pos_x - vert_inters->x)
 			/ tan(DEG2RAD(engine_state->ray_angle)));
-		vert_inters.step_y = -vert_inters.step_y;
+		vert_inters->step_y = -vert_inters->step_y;
 	}
-	// printf("vert_x_step: %g\n", vert_inters.step_x);
-	// printf("vert_y_step: %g\n", vert_inters.step_y);
-	// printf("vert_x:%g\n", vert_inters.x);
-	// printf("vert_y:%g\n", vert_inters.y);
-	return (distance_to_wall(&vert_inters, engine_state, map));
+	// printf("vert_x_step: %g\n", vert_inters->step_x);
+	// printf("vert_y_step: %g\n", vert_inters->step_y);
+	// printf("vert_x:%g\n", vert_inters->x);
+	// printf("vert_y:%g\n", vert_inters->y);
+	return (distance_to_wall(vert_inters, engine_state, map));
 }
 
 void		init_intersection_result(t_intersection_result *inters_result)
@@ -163,7 +159,7 @@ double		wrap_angle(double angle)
 void		render_scene(t_map *map, t_style *style)
 {
 	t_engine_state			engine_state;
-	// t_intersection			intersection;
+	t_intersection			intersection;
 	t_intersection_result	inters_result;
 	double					dist_hor_inters;
 	double					dist_ver_inters;
@@ -180,22 +176,21 @@ void		render_scene(t_map *map, t_style *style)
 		engine_state.ray_angle = start_ray_angle + RAD2DEG(atan((i - style->resolution.x / 2.0)
 			/ engine_state.dist_to_plane)));
 			engine_state.ray_angle = wrap_angle(engine_state.ray_angle);
-			dist_hor_inters = dist_to_hor_inters(&engine_state, map);
-			dist_ver_inters = dist_to_ver_inters(&engine_state, map);
+			dist_hor_inters = dist_to_hor_inters(&engine_state, &intersection, map);
+			dist_ver_inters = dist_to_ver_inters(&engine_state, &intersection, map);
 			// printf("dist_hor_inters: %g\n", dist_hor_inters);
-			printf("dist_ver_inters: %g\n", dist_ver_inters);
+			// printf("dist_ver_inters: %g\n", dist_ver_inters);
 			if (dist_ver_inters < dist_hor_inters)
 			{
 				inters_result.is_side_wall = 1;
-				// if (intersection->) // how to check if the wall is right or left??
+				// if (intersection.step_x < 0)
+					//define texture coordinates, define texture path
 				inters_result.dist_to_wall = dist_ver_inters;
 			}
 			else
 				inters_result.dist_to_wall = dist_hor_inters;
 			// printf("dist_to_wall: %g\n", inters_result.dist_to_wall);
 			// draw a line;
-			printf("ray_angle:%g\n", engine_state.ray_angle);
 			i++;
 	}
-	printf("loop went %d times\n", i);
 }
