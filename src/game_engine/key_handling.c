03332c339906b59a389e3f263da7be6b838bca47
Mollie-S <olcha.olcha@gmail.com>
2021-02-14 15:26:29 +0100

    Destroying window by clicking ESC.

--- src/game_engine/key_handling.c
+++ src/game_engine/key_handling.c
@@ -6,7 +6,7 @@
 /*   By: osamara <osamara@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/09 17:10:15 by osamara       #+#    #+#                 */
-/*   Updated: 2021/02/11 18:57:07 by osamara       ########   odam.nl         */
+/*   Updated: 2021/02/14 15:23:53 by osamara       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,12 @@
 
 #include "mlx.h"
 
+#include <stdlib.h>
+
 int		setup_key_hooks(t_game_engine_state *state)
 {
 	mlx_hook(state->window->mlx_win, 2, 1L << 0, key_pressed, state);
 	mlx_hook(state->window->mlx_win, 3, 1L << 1, key_released, state);
-	// mlx_hook(state->window->mlx_win, 17, 1L << 19, exit_window, state); add exit function
 	return (0);
 }
 
@@ -32,35 +33,40 @@ int		key_pressed(int keycode, t_game_engine_state *state)
 		else
 			state->move.rotation_dir = ROT_RIGHT;
 	}
-	if (keycode == W || keycode == S)
+	else if (keycode == W || keycode == S)
 	{
 		if (keycode == W)
 			state->move.move_ver_dir = MOVE_FORWARD;
 		else
 			state->move.move_ver_dir = MOVE_BACKWARD;
 	}
-	if (keycode == A || keycode == D)
+	else if (keycode == A || keycode == D)
 	{
 		if (keycode == A)
 			state->move.move_hor_dir = MOVE_LEFT;
 		else
 			state->move.move_hor_dir = MOVE_RIGHT;
 	}
+	if (keycode == ESC)
+		exit(0);
 	return (0);
 }
+
 int		key_released(int keycode, t_game_engine_state *state)
 {
 	if (keycode == LEFT || keycode == RIGHT)
 	{
 		state->move.rotation_dir = 0;
 	}
-	if (keycode == W || keycode == S)
+	else if (keycode == W || keycode == S)
 	{
 		state->move.move_ver_dir = 0;
 	}
-	if (keycode == A || keycode == D)
+	else if (keycode == A || keycode == D)
 	{
 		state->move.move_hor_dir = 0;
 	}
+	if (keycode == ESC)
+		exit(0);
 	return (0);
 }
