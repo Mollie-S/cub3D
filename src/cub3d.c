a4b220985299678ad2e1197fa095b06fac0b1136
Mollie-S <olcha.olcha@gmail.com>
2021-02-23 19:02:30 +0100

    Handling incorrect arguments passed.

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,11 +6,11 @@
 /*   By: osamara <osamara@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/10 22:11:26 by osamara       #+#    #+#                 */
-/*   Updated: 2021/02/14 14:57:43 by osamara       ########   odam.nl         */
+/*   Updated: 2021/02/23 18:49:44 by osamara       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <stdio.h> //do I need it here?
 #include <unistd.h>
 #include <stdlib.h> // needed for system( function, remove)
 
@@ -19,6 +19,8 @@
 #include "window.h"
 #include "style.h"
 #include "map.h"
+#include "report_error.h"
+#include "result.h"
 
 // void	free_window(t_window *window)
 // {
@@ -26,37 +28,40 @@
 // 	free(window->mlx_win);
 // }
 
+int	check_arguments(int argc, char **argv)
+{
+	if (argv[1] == NULL)
+		return(report_error("Incorrect filename."));
+	if (argc < 2)
+		return(report_error("Invalid map name."));
+	if (argc > 3)
+		return(report_error("Too many arguments specified."));
+	if (argc == 3 && ft_strncmp("--save", argv[2], 7) != 0)
+		return(report_error("Incorrect argument for creating a screenshot specified."));
+	return (SUCCESS);
+}
+
 int		main(int argc, char **argv)
 {
 	t_style		style;
 	t_map	 	map;
 	t_window	window;
+	int			screenshot;
 
-	if (argv[2])
-	{
-		write(1, "--save", 6);
-	}
-	if (argc < 2)
-	{
-		perror("Please, enter a valid map name");
-		return (1);
-	}
-	if (argc > 2)
-	{
-		perror("Only one map is allowed!");
-		return (1);
-	}
+	if (!check_arguments(argc, argv))
+		return(1);
+	screenshot = FALSE;
+	if (argc == 3)
+		screenshot = TRUE;
 	init_map_style(&style);
 	init_map(&map);
 	if (!parse_cub_map(argv[1], &style, &map))
 	{
-		system("leaks cub3D");
-
 		return (1);
 	}
 	if (!start_window(&window, &style.resolution))
 		return (1);
-	if (!game_loop(&window, &style, &map))
+	if (!game_loop(&window, &style, &map, screenshot))
 		return (1);
 	// check if every mallocced line is freed
 	// debug_print_map(&map); // remove this function!!! 
