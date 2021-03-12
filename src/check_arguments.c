36ae537945ee3dd8e18530b12a3d068ecd7ed2cc
Mollie-S <olcha.olcha@gmail.com>
2021-03-12 22:46:31 +0100

    Updated invalid arguments error message.

--- src/check_arguments.c
+++ src/check_arguments.c
@@ -6,7 +6,7 @@
 /*   By: osamara <osamara@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/01 20:04:15 by osamara       #+#    #+#                 */
-/*   Updated: 2021/03/01 20:11:44 by osamara       ########   odam.nl         */
+/*   Updated: 2021/03/11 23:56:06 by osamara       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@
 
 int	check_arguments(int argc, char **argv)
 {
-	if (argv[1] == NULL)
+	if (argv[0] == NULL)
 		return (report_error("Incorrect filename."));
 	if (argc < 2)
-		return (report_error("Invalid map name."));
+		return (report_error("Please add a map descriptor file."));
 	if (argc > 3)
 		return (report_error("Too many arguments specified."));
 	if (argc == 3 && ft_strncmp("--save", argv[2], 7) != 0)
